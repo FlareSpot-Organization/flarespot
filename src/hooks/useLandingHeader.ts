d8fa3783485863f430a3e825a71a3affd8a6d037
Gospel Jonathan <sirelite11@gmail.com>
2025-01-10 12:46:56 +0100

    fixed build error

--- src/hooks/useLandingHeader.ts
+++ src/hooks/useLandingHeader.ts
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
 
 const useLandingHeader = () => {
   const [showSignInDropdown, setShowSignInDropdown] = useState<boolean>(false);
